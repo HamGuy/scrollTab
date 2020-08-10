2a458e71c88058f6054f69b51fa5a80a1ca28c73
HamGuy <wangrui15@gmail.com>
2020-08-10 09:29:26 +0800

    Initial Commit

---
+++ scrollTab/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  scrollTab
+//
+//  Created by HamGuy on 2020/8/10.
+//  Copyright © 2020 Wedian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
